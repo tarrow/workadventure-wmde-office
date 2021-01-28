6220d7d620fc40cef69d539b50e192060e3f894f
Abban Dunne <abban.dunne@wikimedia.de>
2021-01-28 16:39:23 +0100

    Add new tilesets

---
+++ Office 3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="Office 3" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="tileset3.png" width="320" height="320"/>
+</tileset>

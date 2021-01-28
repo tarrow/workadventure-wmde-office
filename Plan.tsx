b2634387e9516ced37241e25445d8171c705fc54
Abban <Abban@users.noreply.github.com>
2021-01-28 13:51:02 +0000

    Deploying to gh-pages from master @ b799de81bac0bc3e1a239c2f0fa775579e0bc313 🚀

---
+++ Plan.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="Plan" tilewidth="115" tileheight="80" tilecount="56" columns="8">
+ <image source="office.png" width="921" height="633"/>
+</tileset>

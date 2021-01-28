17a55f0a73fe4195000f26931032306709871d80
Abban <Abban@users.noreply.github.com>
2021-01-28 15:39:58 +0000

    Deploying to gh-pages from master @ 6220d7d620fc40cef69d539b50e192060e3f894f 🚀

---
+++ Office 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="Office 1" tilewidth="32" tileheight="32" tilecount="121" columns="11">
+ <image source="tileset1.png" width="352" height="352"/>
+</tileset>
---
+++ Office 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="Office 2" tilewidth="32" tileheight="32" tilecount="120" columns="12">
+ <image source="tileset2.png" width="384" height="320"/>
+</tileset>
